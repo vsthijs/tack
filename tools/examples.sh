6d686a6f1e50abf1fad0f81939720e8db9b3169a
Thijs van Straaten <thijsvstraaten@gmail.com>
2025-03-06 14:50:35 +0100

    Added examples and tools directories

---
+++ tools/examples.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh
+
+if [ ! -d "examples" ]; then
+	>&2 echo "can't find examples directory"
+	exit 1
+fi
+
+if [ ! -e "tack" ]; then
+	>&2 echo "can't find tack compiler"
+	exit 1
+fi
+
+for file in examples/*; do
+	echo "compiling $file"
+	./tack $file
+done
